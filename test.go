4ac2612d6a6cd948a9957d2e83142e9e67f3997d
larago <bingeye@foxmail.com>
2016-11-05 19:22:12 +0800

    first commit

---
+++ test.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a description
+	fmt.Println("Hello, World!")
+}
---
+++ var.go
@@ -0,0 +1,9 @@
+package main
+
+var a = "golang"
+var b string = "cc"
+var c bool
+
+func main() {
+	println(a, b, c)
+}
